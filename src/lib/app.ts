08c4c391cbe7e6efd24e511d042673e9d60edb8b
pedrohenryk91 <ph0639263@gmail.com>
2024-11-11 10:38:42 -0300

    Rotas Post

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -6,13 +6,13 @@ export const app = fastify()
 
 app.get("/",async (request, response)=>{
     console.log(request.ip)
-    response.status(200).send("Paz do Senhor")
+    response.status(200).send("Rodando")
 })
 
-app.post("/adduser",async (request, reponse)=>{
+app.post("/registuser",async (request, reponse)=>{
 
     const {Email, Password, Username} = z.object({
-        Email:z.string(),
+        Email:z.string().email(),
         Password:z.string(),
         Username:z.string().optional()
     }).parse(request.body)
@@ -24,12 +24,65 @@ app.post("/adduser",async (request, reponse)=>{
     })
 
     if(obj_user){
-        reponse.status(201).send("Usuario Criado" + obj_user)
+        reponse.status(201).send({
+            Description: "Usuario Criado",
+            User_Id: obj_user.Id,
+            User_Name: Username,
+            User_Email: Email
+        })
     }
 })
 
-app.post("/action/:value",async (request, response)=>{
-    
+app.post("/action/:UserId/:Value/:Type",async (request, response)=>{
+
+    const {UserId,Value,Type} = z.object({
+        UserId: z.string().refine((value)=>{
+            return !isNaN(Number(value))
+        }),
+        Value: z.string().refine((value)=>{
+            return !isNaN(Number(value))
+        }),
+        Type: z.string()
+    }).parse(request.params)
+
+    const obj_transaction = prisma.transaction.create({
+        data:{
+            Value: Number(Value),
+            UserId: Number(UserId),
+            Type
+        }
+    })
+
+    response.status(201).send({
+        Description: "Registro de Transação",
+        Value: (await obj_transaction).Value,
+        Type: (await obj_transaction).Type
+    })
+})
+
+app.patch("/auth/login",async(req,res)=>{
+
+    const {Email,Password} = z.object({
+        Email:z.string().email(),
+        Password:z.string()
+    }).parse(req.body)
+
+    const doesTheEmailExists = await prisma.user.findUnique({
+        where:{
+            Email
+        }
+    })
+
+    if(!doesTheEmailExists){
+        res.status(404).send("Email não encontrado")
+    } else {
+        if(Password==doesTheEmailExists.Password){
+            res.status(200).send({
+                Description:"Usuário logado",
+                UserId:doesTheEmailExists.Id
+            })
+        }
+    }
 })
 
 app.delete("/remuser/:Id", async (request, response)=>{
